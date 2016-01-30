4a878a62cfb4f1006591fe3d0e035498f1ce79c1
myfriendkang <myfriendkang@gmail.com>
2016-01-31 05:10:53 +0900

    Add Audio and Arduino
    add audio manager and Arduino

---
+++ Assets/2. Script/Arduino.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class Arduino : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/2. Script/DistortAudio.cs
+++ Assets/2. Script/DistortAudio.cs
@@ -1,15 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(AudioSource))]
 public class DistortAudio : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    #region Private variables
+    public float startingPitch = 1.0f;
+    public float maxPitch = 2.0f;
+    #endregion
+
+    #region Public variables
+    private float count = 1.0f;
+    private AudioSource voiceAudio;
+    private float volume;
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+        voiceAudio = GetComponent<AudioSource>();
+        voiceAudio.pitch = startingPitch;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if (count <= maxPitch)
+            {
+                voiceAudio.pitch = count;
+                Debug.Log(voiceAudio);
+                count += 0.2f;
+            }
+            else
+            {
+                count = maxPitch;
+            }
+                voiceAudio.Play();
+            }
 	}
 }
