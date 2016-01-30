7a6879253aa83c3d0ef357c73a3f627f43c51e5d
myfriendkang <myfriendkang@gmail.com>
2016-01-31 06:41:36 +0900

    Add Arduino code

--- Assets/2. Script/Arduino.cs
+++ Assets/2. Script/Arduino.cs
@@ -1,16 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.IO;
+using System.IO.Ports;
 
 public class Arduino : MonoBehaviour {
 
+    SerialPort sp = new SerialPort("COM3", 9600);
+    
+    private int buttonCount = 0;
+    
 	// Use this for initialization
 	void Start () {
-	
+        if (sp.IsOpen)
+        {
+            sp.Close();
+        }
+        else {
+            sp.Open();
+            sp.ReadTimeout = 1;
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+    public int GetData()
+    {
+        return buttonCount;
+    }
+    void SetData(int temp)
+    {
+        buttonCount = temp;
+    }
+
+    void Update() {
+        if (sp.IsOpen)
+        {
+            try
+            {
+                Debug.Log(sp.ReadByte());
+            }
+            catch (System.Exception)
+            {
+               
+            }
+        }
 	}
 }
