77076dfeec94e6bd9f1785d1220dbbcc7167c09e
myfriendkang <myfriendkang@gmail.com>
2016-01-31 02:46:33 +0900

    change Camera Change
    delet itween and put camera change

---
+++ Assets/2. Script/CameraFlying.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFlying : MonoBehaviour {
+
+    #region animation, transform value
+    private Animation _anim;
+    private Vector3 _fristPosition;
+    private Vector3 _rotation;
+    #endregion
+
+    void Start()
+    {
+        _anim = GetComponent<Animation>();
+        _fristPosition = transform.position;
+        _rotation = transform.rotation.eulerAngles;
+    }
+
+	void Update () {
+        if (Input.GetKey(KeyCode.F))
+        {
+            _anim.Play();
+        }
+        else if (Input.GetKey(KeyCode.G))
+        {
+            transform.position= new Vector3(_fristPosition.x, _fristPosition.y, _fristPosition.z);
+            transform.rotation = Quaternion.Euler(_rotation.x, _rotation.y, _rotation.z);
+        }
+	}
+}
